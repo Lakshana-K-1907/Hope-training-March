ba798f9b5a4e719942b61c7072ff60c03cf23116
Lakshana K <kleela0407@gmail.com>
2026-04-05 12:02:18 +0530

    Create BankAccount.java

---
+++ Day10/BankAccount.java
@@ -0,0 +1,25 @@
+package Day10;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
