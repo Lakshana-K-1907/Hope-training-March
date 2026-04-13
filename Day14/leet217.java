b053eb11c5e1d13239d0c18d99478207c965231d
Lakshana K <kleela0407@gmail.com>
2026-04-13 12:23:59 +0530

    Implement containsDuplicate method in leet217

---
+++ Day14/leet217.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer>s=new HashSet<>();
+        for(int n:nums){
+            s.add(n);
+        }
+        return(nums.length!=s.size());
+        
+    }
+}
